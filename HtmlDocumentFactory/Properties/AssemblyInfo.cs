b314c3a5e4f14925e89440b73af69d1769eeec03
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2018-05-19 21:31:30 +0500

    Add files via upload

---
+++ HtmlDocumentFactory/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HtmlDocumentFactory")]
+[assembly: AssemblyDescription("Helper library for creating HTML with WinForms HtmlDocument class")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Private person")]
+[assembly: AssemblyProduct("HtmlDocumentFactory")]
+[assembly: AssemblyCopyright("Copyright © MSDN.WhiteKnight 2018")]
+[assembly: AssemblyTrademark("HtmlDocumentFactory")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f6617361-a707-4001-b351-e292566369d2")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
